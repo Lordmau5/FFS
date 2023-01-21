288b79b85645c4d1d9cb7f2bb9384fa83d1ac330
Lordmau5 <mail@lordmau5.com>
2023-01-21 13:55:54 +0100

    Fix T.I.T. sounds

--- src/main/java/com/lordmau5/ffs/item/ItemTit.java
+++ src/main/java/com/lordmau5/ffs/item/ItemTit.java
@@ -2,10 +2,13 @@ package com.lordmau5.ffs.item;
 
 import com.lordmau5.ffs.FancyFluidStorage;
 import com.lordmau5.ffs.holder.Sounds;
+import net.minecraft.client.audio.SoundSource;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemUseContext;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Hand;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.world.World;
@@ -17,11 +20,23 @@ public class ItemTit extends Item {
         super(properties.tab(FancyFluidStorage.ITEM_GROUP));
     }
 
-    @Override
-    public ActionResult<ItemStack> use(World worldIn, PlayerEntity playerIn, Hand handIn) {
+    private void playSound(World level, PlayerEntity player) {
         float randomPitch = 0.75f + (new Random().nextFloat() * 0.5f);
-        worldIn.playSound(playerIn, playerIn.getX(), playerIn.getY(), playerIn.getZ(), Sounds.birdSounds, SoundCategory.AMBIENT, 1.0f, randomPitch);
+        level.playSound(player, player.getX(), player.getY(), player.getZ(), Sounds.birdSounds, SoundCategory.AMBIENT, 1.0f, randomPitch);
+    }
 
-        return super.use(worldIn, playerIn, handIn);
+    @Override
+    public ActionResultType onItemUseFirst(ItemStack stack, ItemUseContext context) {
+        if (context.getLevel().getBlockEntity(context.getClickedPos()) != null)
+            playSound(context.getLevel(), context.getPlayer());
+
+        return super.onItemUseFirst(stack, context);
+    }
+
+    @Override
+    public ActionResult<ItemStack> use(World level, PlayerEntity player, Hand hand) {
+        playSound(level, player);
+
+        return super.use(level, player, hand);
     }
 }
